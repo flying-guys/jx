3274dd6e0f2c9c38706501b09d2d559c456c7b25
flying-guys <88775801+flying-guys@users.noreply.github.com>
2022-08-06 20:24:20 +0800

    Add files via upload

---
+++ tvbox.php
@@ -0,0 +1,7 @@
+
+<?php  
+$url = "https://agit.ai/lineagetmy/test/raw/branch/master/config.json";
+echo "<script type='text/javascript'>";
+echo "window.location.href='$url'";
+echo "</script>"; 
+?>
